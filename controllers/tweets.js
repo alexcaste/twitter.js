3109e56ba54952f874af034bf28d6c69e604b74f
alex caste & frank <student@epicodus.com>
2015-06-09 10:53:59 -0700

    add initial files

---
+++ controllers/tweets.js
@@ -0,0 +1,11 @@
+Twitter.TweetsController = Ember.ArrayController.extend({
+  isEditing: false,
+  actions: {
+    edit: function() {
+      this.set('isEditing', true);
+    },
+    save: function() {
+      this.set('isEditing', false);
+    }
+  }
+});
---
+++ store.js
@@ -0,0 +1,14 @@
+var tweets = [
+  {
+    id: '1',
+    body: "tweet tweet celebrity bullshit"
+  },
+  {
+    id: '2',
+    body: "the president got laid"
+  },
+  {
+    id: '3',
+    body: "here is a picture of my lunch"
+  }
+];
