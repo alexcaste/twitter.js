a0075a5016cffe57faeb100eb44c31bc1a7b13ca
alex caste & frank <student@epicodus.com>
2015-06-09 16:55:49 -0700

    allow replies

--- controllers/tweets.js
+++ controllers/tweets.js
@@ -1,8 +1,13 @@
 Twitter.TweetsController = Ember.Controller.extend({
+  clicked: false,
   sortProperties: ['date'],
   tweets: Ember.computed.sort('model','sortProperties'),
 
   actions: {
+    isClicked: function() {
+      this.set('clicked', !this.get('clicked'));
+    },
+
     addReply: function(tweet) {
       tweet.comments.addObject(tweet.reply);
     }
--- store.js
+++ store.js
@@ -1,14 +1,17 @@
 var tweets = [
   {
     date: new Date('Thu, 01 Jan 1979 00:00:00 GMT-0400'),
-    text: "tweet tweet celebrity bullshit"
+    text: "tweet tweet celebrity bullshit",
+    comments: []
   },
   {
     date: new Date('Thu, 01 Jan 1973 00:00:00 GMT-0400'),
-    text: "the president got laid"
+    text: "the president got laid",
+    comments: []
   },
   {
     date: new Date('Thu, 01 Jan 1975 00:00:00 GMT-0400'),
-    text: "here is a picture of my lunch"
+    text: "here is a picture of my lunch",
+    comments: []
   }
 ];
